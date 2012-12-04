fb2d115f456f11525dbfbacff0c2dde522861fd7
Marcus M. Darden <ontoral@gmail.com>
2012-12-04 07:54:07 -0500

    Use environment variables and some linting.

--- showrss.py
+++ showrss.py
@@ -5,11 +5,11 @@ import datetime, time
 from sqlite3 import dbapi2 as sqlite3
 import urllib
 
-FEED = 'http://showrss.karmorra.info/rss.php?user_id=94858&hd=0&proper=1'
+SHOWRSS_FEED = 'http://showrss.karmorra.info/rss.php?user_id=94858&hd=0&proper=1'
 TORRENT_PATH = 'torrents/'
 
 class Entry(object):
-    filename_template = '{0.showname}.S{0.season:02d}E{0.episode:02d}.torrent'
+    filename_template = '{showname}.S{season:02d}E{episode:02d}.torrent'
     def __init__(self, entry):
         m = re.search('(.*) [sS]?([0-9]{1,2})[eExX]([0-9]{1,2}) .*', entry.title)
         self.id = entry.link.split('/')[-1].split('.')[0]
@@ -17,18 +17,20 @@ class Entry(object):
         self.showname = m.group(1)
         self.season = int(m.group(2))
         self.episode = int(m.group(3))
-        self.filename = self.filename_template.format(self).replace(' ', '.')
+        self.filename = self.filename_template.format(**self.__dict__).replace(' ', '.')
         self.url = entry.link
 
 def main():
-    d = feedparser.parse(FEED)
+    d = feedparser.parse(os.environ.get('SHOWRSS_FEED', SHOWRSS_FEED))
 
     for ee in d.entries:
         entry = Entry(ee)
-        filename = os.path.join(os.environ.get('TORRENT_PATH', TORRENT_PATH), entry.filename)
+        filename = os.path.join(os.environ.get('TORRENT_PATH', TORRENT_PATH),
+                                entry.filename)
         urllib.urlretrieve(entry.url, filename)
 
-        print 'Downloaded torrent file: {0.showname}, Season {0.season} - Episode {0.episode}'.format(entry)
+        msg = 'Downloaded torrent file: S{season:02d}E{episode:02d} - {showname}'
+        print msg.format(**entry.__dict__)
 
 
 if __name__ == '__main__':
