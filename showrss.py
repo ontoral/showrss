ada5da48fa904c23c77881e5ac9916fa1d6b22ca
Marcus M. Darden <ontoral@gmail.com>
2012-12-04 07:32:58 -0500

    Use a class (Entry) to manage feed info.

--- showrss.py
+++ showrss.py
@@ -1,3 +1,4 @@
+import os
 import feedparser
 import re
 import datetime, time
@@ -5,22 +6,29 @@ from sqlite3 import dbapi2 as sqlite3
 import urllib
 
 FEED = 'http://showrss.karmorra.info/rss.php?user_id=94858&hd=0&proper=1'
+TORRENT_PATH = 'torrents/'
 
-def get_show_info(title):
-    m = re.search('(.*) [sS]?([0-9]{1,2})[eExX]([0-9]{1,2}) .*', title)
-    return dict(showname=m.group(1), season=int(m.group(2)), episode=int(m.group(3)))
+class Entry(object):
+    filename_template = '{0.showname}.S{0.season:02d}E{0.episode:02d}.torrent'
+    def __init__(self, entry):
+        m = re.search('(.*) [sS]?([0-9]{1,2})[eExX]([0-9]{1,2}) .*', entry.title)
+        self.id = entry.link.split('/')[-1].split('.')[0]
+        self.date = int(time.mktime(entry.published_parsed))
+        self.showname = m.group(1)
+        self.season = int(m.group(2))
+        self.episode = int(m.group(3))
+        self.filename = self.filename_template.format(self).replace(' ', '.')
+        self.url = entry.link
 
 def main():
     d = feedparser.parse(FEED)
 
-    for entry in d.entries:
-        info = get_show_info(entry.title)
-        info['link'] = entry.link
-        info['published'] = datetime.datetime.fromtimestamp(time.mktime(entry.published_parsed))
-        info['filename'] = entry.link.split('/')[-1]
-        urllib.urlretrieve(entry.link, info['filename'])
+    for ee in d.entries:
+        entry = Entry(ee)
+        filename = os.path.join(os.environ.get('TORRENT_PATH', TORRENT_PATH), entry.filename)
+        urllib.urlretrieve(entry.url, filename)
 
-        print info
+        print 'Downloaded torrent file: {0.showname}, Season {0.season} - Episode {0.episode}'.format(entry)
 
 
 if __name__ == '__main__':
