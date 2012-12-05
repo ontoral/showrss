846333c4e3f044d4ccf809e119646355cb1c6843
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2012-12-05 03:47:27 -0500

    Added timestamping.
    To prevent the downloader from repeatedly downloading the same torrent
    files, the published time of the newest posted feed item is returned
    from the main function. When the module runner is called, this timestamp
    is stored in and read from a "settings" file (.showrss by default).

--- showrss.py
+++ showrss.py
@@ -13,25 +13,47 @@ class Entry(object):
     def __init__(self, entry):
         m = re.search('(.*) [sS]?([0-9]{1,2})[eExX]([0-9]{1,2}) .*', entry.title)
         self.id = entry.link.split('/')[-1].split('.')[0]
-        self.date = int(time.mktime(entry.published_parsed))
+        self.timestamp = int(time.mktime(entry.published_parsed))
         self.showname = m.group(1)
         self.season = int(m.group(2))
         self.episode = int(m.group(3))
         self.filename = self.filename_template.format(**self.__dict__).replace(' ', '.')
         self.url = entry.link
 
-def main():
+def main(timestamp=0):
     d = feedparser.parse(os.environ.get('SHOWRSS_FEED', SHOWRSS_FEED))
 
+    newstamp = timestamp
     for ee in d.entries:
         entry = Entry(ee)
-        filename = os.path.join(os.environ.get('TORRENT_PATH', TORRENT_PATH),
-                                entry.filename)
-        urllib.urlretrieve(entry.url, filename)
+        if entry.timestamp > timestamp:
+            newstamp = max(newstamp, entry.timestamp)
+            filename = os.path.join(os.environ.get('TORRENT_PATH',
+                                                   TORRENT_PATH),
+                                    entry.filename)
+            urllib.urlretrieve(entry.url, filename)
+            msg = 'Downloaded torrent file: S{season:02d}E{episode:02d} - {showname}'
+        else:
+            msg = 'Skipping: S{season:02d}E{episode:02d} - {showname}'
 
-        msg = 'Downloaded torrent file: S{season:02d}E{episode:02d} - {showname}'
         print msg.format(**entry.__dict__)
+
+    print datetime.datetime.fromtimestamp(newstamp)
+    return newstamp
 
 
 if __name__ == '__main__':
-    main()
+    # Ensure base timestamp (epoch) exists
+    if not os.path.exists('.showrss'):
+        with file('.showrss', 'w') as settings:
+            write(str(0))
+
+    # Read timestamp for most recently posted feed item
+    with file('.showrss', 'r') as settings:
+        timestamp = int(settings.readline())
+
+    timestamp = main(timestamp)
+
+    # Record the most recent feed item in a settings file
+    with file('.showrss', 'w') as settings:
+        settings.write(str(timestamp))
