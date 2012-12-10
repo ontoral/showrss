3496dae9a445f8054c9ae37e6795dffaf26cedb9
Marcus M. Darden <ontoral@gmail.com>
2012-12-10 01:13:47 -0500

    A script run upon torrent download completition.

---
+++ torrentdone.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os
+
+with file('/home/pi/showrss/torrent-done.log', 'a') as f:
+    # f.write(os.environ.get('TR_APP_VERSION', 'TR_APP_VERSION')+'\n')
+    # Version 2.52
+    f.write(os.environ.get('TR_TIME_LOCALTIME', 'TR_TIME_LOCALTIME')+'\n')
+    f.write(os.environ.get('TR_TORRENT_DIR', 'TR_TORRENT_DIR')+'\n')
+    f.write(os.environ.get('TR_TORRENT_HASH', 'TR_TORRENT_HASH')+'\n')
+    f.write(os.environ.get('TR_TORRENT_ID', 'TR_TORRENT_ID')+'\n')
+    f.write(os.environ.get('TR_TORRENT_NAME', 'TR_TORRENT_NAME')+'\n')
+
