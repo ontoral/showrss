0076cdb0fbce72efefee21c8eaaf735a7ecbcc96
Marcus M. Darden <ontoral@gmail.com>
2013-01-20 23:55:48 -0500

    Linting.

--- showrss.py
+++ showrss.py
@@ -1,15 +1,18 @@
 import os
 import feedparser
 import re
-import datetime, time
-from sqlite3 import dbapi2 as sqlite3
+import datetime
+import time
+# from sqlite3 import dbapi2 as sqlite3
 import urllib
 
 SHOWRSS_FEED = 'http://showrss.karmorra.info/rss.php?user_id=94858&hd=0&proper=1'
 TORRENT_PATH = 'torrents/'
 
+
 class Entry(object):
     filename_template = '{showname}.S{season:02d}E{episode:02d}.torrent'
+
     def __init__(self, entry):
         m = re.search('(.*) [sS]?([0-9]{1,2})[eExX]([0-9]{1,2}) .*', entry.title)
         self.hash = entry.link.split('/')[-1].split('.')[0]
@@ -19,6 +22,7 @@ class Entry(object):
         self.episode = int(m.group(3))
         self.filename = self.filename_template.format(**self.__dict__).replace(' ', '.')
         self.url = entry.link
+
 
 def main(timestamp=0):
     d = feedparser.parse(os.environ.get('SHOWRSS_FEED', SHOWRSS_FEED))
@@ -46,7 +50,7 @@ if __name__ == '__main__':
     # Ensure base timestamp (epoch) exists
     if not os.path.exists('.showrss'):
         with file('.showrss', 'w') as settings:
-            write(str(0))
+            settings.write(str(0))
 
     # Read timestamp for most recently posted feed item
     with file('.showrss', 'r') as settings:
