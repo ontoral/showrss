2529c7d7bbf28b53f8268f187481af9e91a8fb01
Marcus M. Darden <ontoral@gmail.com>
2013-11-27 09:09:51 -0500

    Update Showrss feed address.

--- showrss.py
+++ showrss.py
@@ -15,28 +15,33 @@ import feedparser
 
 
 # Config
-SHOWRSS_FEED = 'http://showrss.karmorra.info/rss.php?user_id=94858&hd=0&proper=1&magnets=true'
+SHOWRSS_FEED = 'http://showrss.info/rss.php?user_id=94858&hd=0&proper=1'
 SHOWRSS_FEED = os.environ.get('SHOWRSS_FEED', SHOWRSS_FEED)
 SHOWRSS_TIMESTAMP = os.environ.get('SHOWRSS_TIMESTAMP', os.path.expanduser('~/.timestamp'))
-SHOWRSS_DEBUG = os.environ.get('SHOWRSS_DEBUG', True)
+SHOWRSS_DEBUG = bool(os.environ.get('SHOWRSS_DEBUG', False))
 
 
 class Entry(object):
     def __init__(self, entry):
-#        print 'entry:', len(entry), entry.keys(), '\n', entry
-        m = re.search('(.*) [sS]?([0-9]{1,2})([eExX]([0-9]{1,2}))* .*', entry.title)
+        #print 'entry:', len(entry), entry.keys(), '\n', entry
+        #print entry.title
+        regexp = '(.*)[ .-][sS]?([0-9]{1,2})[eExX]([0-9]{1,2})*.*'
+        m = re.search(regexp, entry.title)
         self.timestamp = int(time.mktime(entry.published_parsed))
         self.showname = m.group(1)
         self.season = 0
         self.episode = 0
         try:
             self.season = int(m.group(2))
-            self.episode = int(m.group(3)[-2:])
+            self.episode = int(m.group(3))
         except:
             pass
         m = re.search('.*\?xt=([^&]*).*', entry.link)
         self.hash = m.group(1)
         self.url = entry.link
+
+    def __repr__(self):
+        return '<Magnet: {0.hash}, {0.showname} S{0.season:02d}E{0.episode:02d}>'.format(self)
 
 
 def main(timestamp):
@@ -45,6 +50,7 @@ def main(timestamp):
     newstamp = timestamp
     for ee in d.entries:
         entry = Entry(ee)
+        print entry
         if entry.timestamp > timestamp:
             newstamp = max(newstamp, entry.timestamp)
             subprocess.call(['transmission-remote', '-a', entry.url])
@@ -67,9 +73,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=description)
 
     try:
-        # trremote = subprocess.call(['transmission-remote', '-si'], stdout=tf)
-        trremote = subprocess.call(['transmission-remote', '-si'])
-        print trremote
+        session_info = subprocess.check_output(['transmission-remote',
+                                                '-si'])
+        download_dir = re.search('Download directory: ([^\s]*)',
+                                 session_info)
     except OSError, e:
         print 'Transmission daemon not found.'
 
